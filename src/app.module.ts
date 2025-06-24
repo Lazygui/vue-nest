3024947e1b01c2c7bbf85218c59f8aa12ef66d8b
zhouzhijun <15642532452@163.com>
2025-06-24 15:50:20 +0800

    refactor: 删除无用的 auth 模块代码
    - 移除了 auth 模块的全部代码，包括控制器、服务、模块定义等
    - 删除了与 auth 相关的所有测试文件
    - 移除了 app.module.ts 中对 auth 模块的引用

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { UserModule } from "@controller/user/user.module"
 import { HtmlModule } from './html/html.module';
 import { ConfigModule } from './config/config.module';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from '@controller/auth/auth.module';
 
 @Module({
   imports: [
